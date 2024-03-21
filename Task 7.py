07f62cd78b40ff3231e5cc13cd256fa4eee244cb
kost960 <pozitq3@gmail.com>
2024-03-21 21:28:58 +0700

    All tasks

--- Task 7.py
+++ Task 7.py
@@ -1,8 +1,25 @@
 quantity = input('Введите данные станций (N, K, M соответственно): ')
 
 quantity_split = quantity.split()
+N = int(quantity_split[0])
+K = int(quantity_split[1])
+M = int(quantity_split[2])
+count = 0
 
-if int(quantity_split[1]) < int(quantity_split[2]):
-    print(abs(int(quantity_split[2]) - int(quantity_split[1])))
+if M > K and M - K <= N // 2 or M < K and K - M > N // 2:
+    K += 1
+    while K != M:
+        count += 1
+        if K == N:
+            K = 0
+        K += 1
+elif K == M or abs(K - M) == 1:
+    pass
 else:
-    print(int(quantity_split[2]) - abs(int(quantity_split[1]) + (int(quantity_split[0])) - 1))
+    quantity_split[1] -= 1
+    while K != M:
+        count += 1
+        if K <= 0:
+            K == N
+        K -= 1
+print(count)
--- Task 9.py
+++ Task 9.py
@@ -13,8 +13,7 @@ if int(numbers[0]) > int(numbers[1]):
 else:
     if int(numbers[0]) > int(numbers[2]):
         print(f'{numbers[1]} {numbers[0]} {numbers[2]}')
+    elif int(numbers[1]) > int(numbers[2]):
+        print(f'{numbers[1]} {numbers[2]} {numbers[0]}')
     else:
-        if int(numbers[1]) > int(numbers[2]):
-            print(f'{numbers[1]} {numbers[2]} {numbers[0]}')
-        else:
-            print(f'{numbers[2]} {numbers[1]} {numbers[0]}')
+        print(f'{numbers[2]} {numbers[1]} {numbers[0]}')
